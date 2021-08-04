676b0a037044be0ba74e14d6fbd4165e07b6b9f8
Abdulfetah Jemal <32792371+Abdulfetahjemal@users.noreply.github.com>
2021-08-04 19:15:05 +0300

    Done Uploading

--- Weather/AppShell.xaml.cs
+++ Weather/AppShell.xaml.cs
@@ -11,8 +11,6 @@ namespace Weather
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
             Routing.RegisterRoute(nameof(DetailPage), typeof(DetailPage));
         }
 
