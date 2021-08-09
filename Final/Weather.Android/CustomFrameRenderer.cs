d4d419191a22c080cd21dac3da850b0ed5d95ea2
Abdulfetah Jemal <32792371+Abdulfetahjemal@users.noreply.github.com>
2021-08-09 16:42:41 +0300

    CustomRenderer Added

---
+++ Final/Weather.Android/CustomFrameRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Weather;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Weather.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomFrame), typeof(CustomFrameRenderer))]
+namespace Weather.Droid
+{
+    public class CustomFrameRenderer : FrameRenderer
+    {
+        public CustomFrameRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement.HasShadow)
+            {
+                SetOutlineSpotShadowColor(Android.Graphics.Color.White);
+                Elevation = 50.0f;
+
+
+            }
+        }
+    }
+}
---
+++ Final/Weather.iOS/CustomFrameRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using UIKit;
+using Weather;
+using Weather.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomFrame), typeof(CustomFrameRenderer))]
+namespace Weather.iOS
+{
+    class CustomFrameRenderer : FrameRenderer
+    {
+
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+            base.LayoutSubviews();
+            this.Layer.ShadowRadius = 2.0f;
+            this.Layer.ShadowColor = UIColor.Red.CGColor;
+         
+        }
+    }
+}
